641bd9aaf3ae3ae686a6bac48cebed8e6225f9e1
Yannick Schall <yannick@bishbashbosh.it>
2014-05-30 12:32:10 +0100

    add npm path module to handle less  import

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,19 @@
-// npm install gulp gulp-less gulp-watch gulp-autoprefixer gulp-plumber gulp-livereload gulp-less --save-dev
+// npm install gulp gulp-less gulp-watch gulp-autoprefixer gulp-plumber gulp-livereload gulp-less path --save-dev
 var gulp = require('gulp');
 var less = require('gulp-less');
 var watch = require('gulp-watch');
 var prefix = require('gulp-autoprefixer');
 var plumber = require('gulp-plumber');
 var livereload = require('gulp-livereload');
+var path = require('path');
 
 gulp.task('default', function() {
-    return gulp.src('./*.less')
+    return gulp.src('./style.less')
         .pipe(watch())
         .pipe(plumber())
-        .pipe(less())
+        .pipe(less({
+          paths: ['./', './overrides/']
+        } ))
         .pipe(prefix("last 8 version", "> 1%", "ie 8", "ie 7"), {cascade:true})
         .pipe(gulp.dest('./'))
         .pipe(livereload());
