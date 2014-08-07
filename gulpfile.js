dd1ef5b3d7bbbd6fcd2cf0a1531fb3ad1e127257
Yannick Schall <yannick@bishbashbosh.it>
2014-08-07 13:26:11 +0100

    test merge mediaqueries, css re-ordering and compression

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,16 @@
 // npm install gulp gulp-less gulp-watch gulp-autoprefixer gulp-plumber gulp-livereload gulp-less path --save-dev
 // explanation task breakdown: http://stackoverflow.com/questions/23953779/gulp-watch-and-compile-less-files-with-import
-var gulp = require('gulp');
-var less = require('gulp-less');
-var watch = require('gulp-watch');
-var prefix = require('gulp-autoprefixer');
-var plumber = require('gulp-plumber');
-var minifycss = require('gulp-minify-css');
-// var uglify = require('gulp-uglify');
-var livereload = require('gulp-livereload');
-var path = require('path');
+var gulp = require('gulp'),
+less = require('gulp-less'),
+watch = require('gulp-watch'),
+prefix = require('gulp-autoprefixer'),
+plumber = require('gulp-plumber'),
+livereload = require('gulp-livereload'),
+colorguard = require('gulp-colorguard'),
+cmq = require('gulp-combine-media-queries'),
+minifyCSS = require('gulp-minify-css'),
+csscomb = require('gulp-csscomb'),
+path = require('path');
 
 gulp.task('less', function() {
     gulp.src('./style.less')  // only compile the entry file
@@ -16,9 +18,17 @@ gulp.task('less', function() {
         .pipe(less({
           paths: ['./', './partials/', './overrides/']
         } ))
-        .pipe(prefix("last 8 version", "> 1%", "ie 9", "ie 8", "ie 7"), {cascade:true})
+        .pipe(prefix("last 2 version", "> 1%", "ie 9", "ie 8", "ie 7"))
+        .pipe(cmq({
+          log: true
+        }))
+        .pipe(colorguard())
+        .pipe(csscomb())
+        .pipe(minifyCSS({
+          // noAdvanced: true,
+          keepBreaks: true
+        }))
         .pipe(plumber.stop())
-        .pipe(minifycss({keepBreaks: true}))
         .pipe(gulp.dest('./'))
         .pipe(livereload());
 });
@@ -26,4 +36,4 @@ gulp.task('watch', function() {
     gulp.watch('./**/*.less', ['less']);  // Watch all the .less files, then run the less task
 });
 
-g
+gulp.task('default', ['watch']); // Default will run the 'entry' watch task
